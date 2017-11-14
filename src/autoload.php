3e9e573c4bae176ce422cb9ccee148794b2be334
Stefan Schomacker <stefan.scho.android@gmail.com>
2017-11-14 20:52:30 +0100

    add vendor autoload

--- src/autoload.php
+++ src/autoload.php
@@ -1,4 +1,5 @@
 <?php
+require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 spl_autoload_register(function ($class) {
     $class = str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
     require_once(dirname(__FILE__) . DIRECTORY_SEPARATOR . $class);
